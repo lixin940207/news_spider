e2688ed933f3d230ed2804e4a8d925d58f6418b3
lixin-940207 <alix.xinli@gmail.com>
2022-11-13 23:48:55 +0100

    add cron schedule for each news crawling

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const fork = require("child_process").fork;
 
-// fork('./services/bbc/fetch_latest_service.js');
-// fork('./services/nytimes/fetch_latest_service.js');
-// fork('./services/bfm/fetch_latest_service.js');
-// fork('./services/france24/fetch_latest_service.js');
+fork('./services/bbc/fetch_latest_service.js');
+fork('./services/nytimes/fetch_latest_service.js');
+fork('./services/bfm/fetch_latest_service.js');
+fork('./services/france24/fetch_latest_service.js');
 fork('./services/lefigaro/fetch_latest_service.js');
 fork('./services/lemonde/fetch_latest_service.js');
 fork('./services/leparisien/fetch_latest_service.js');
-
-// fork('./services/bbc/fetch_china_service.js');
-// fork('./services/nytimes/fetch_china_service.js');
-// fork('./services/bfm/fetch_china_service.js');
-// fork('./services/france24/fetch_china_service.js');
-// fork('./services/lefigaro/fetch_china_service.js');
-// fork('./services/lemonde/fetch_china_service.js');
-// fork('./services/leparisien/fetch_china_service.js');
-
-
-
-
+//
+fork('./services/bbc/fetch_china_service.js');
+fork('./services/nytimes/fetch_china_service.js');
+fork('./services/bfm/fetch_china_service.js');
+fork('./services/france24/fetch_china_service.js');
+fork('./services/lefigaro/fetch_china_service.js');
+fork('./services/lemonde/fetch_china_service.js');
+fork('./services/leparisien/fetch_china_service.js');
+//
+fork('./services/bbc/fetch_tech_service.js');
+fork('./services/techcrunch/fetch_tech_service.js');
+fork('./services/wired/fetch_tech_service.js');
